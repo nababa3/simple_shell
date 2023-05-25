9ee90ac1426a7358259d10a940b4eafda1eae38a
“Nababa <nababa3@github.com>
2023-05-25 16:54:33 -0700

    review

--- exits.c
+++ exits.c
@@ -1,16 +1,17 @@
 #include "shell.h"
 
 /**
- * _strncpy - copies a string
- * @dest: the destination string to be copied to
- * @src: the source string
- * @n: the amount of characters to be copied
- * Return: the concatenated string
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *
+ *Return: the concatenated string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
-	char *str = dest;
+	char *s = dest;
 
 	i = 0;
 	while (src[i] != '\0' && i < n - 1)
@@ -27,20 +28,21 @@ char *_strncpy(char *dest, char *src, int n)
 			j++;
 		}
 	}
-	return (str);
+	return (s);
 }
 
 /**
- * _strncat - concatenates two strings
- * @dest: the first string
- * @src: the second string
- * @n: the amount of bytes to be maximally used
- * Return: the concatenated string
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *
+ *Return: the concatenated string
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
-	char *str = dest;
+	char *s = dest;
 
 	i = 0;
 	j = 0;
@@ -54,14 +56,15 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	if (j < n)
 		dest[i] = '\0';
-	return (str);
+	return (s);
 }
 
 /**
- * _strchr - locates a character in a string
- * @s: the string to be parsed
- * @c: the character to look for
- * Return: (s) a pointer to the memory area s
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *
+ *Return: (s) a pointer to the memory area s
  */
 char *_strchr(char *s, char c)
 {
@@ -72,4 +75,3 @@ char *_strchr(char *s, char c)
 
 	return (NULL);
 }
-
